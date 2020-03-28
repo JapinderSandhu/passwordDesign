789a79b1c4050b68f4588cfd28a227a91ccf51a2
JapinderSandhu <salutations@japi.ca>
2020-03-27 21:45:08 -0400

    Updated Frontend :cosmetic:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Password Input Program`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Japinder Sandhu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,21 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    
+    <button class="button button5"></button>
+    <button class="button button5"></button>
+    <button class="button button5"></button>
+    <button class="button button5"></button>
+    <button class="button button5"></button>
+    <button class="button button5"></button>
+    <button class="button button5"></button>
+    <button class="button button5"></button>
+    <button class="button button5"></button>
+    <button class="button button5"></button>
+    <button class="button button5"></button>
+    <button class="button button5"></button>
+
+
   </Layout>
 )
 
